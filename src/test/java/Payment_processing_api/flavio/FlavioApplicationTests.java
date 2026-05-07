f4141818c4671d1042d2137f3d136a34dd9803a5
FL4V10ARC <flaviocarvalho9029@gmail.com>
2026-05-07 12:58:52 -0400

    chore: initialize Spring Boot payment processing API

---
+++ src/test/java/Payment_processing_api/flavio/FlavioApplicationTests.java
@@ -0,0 +1,13 @@
+package Payment_processing_api.flavio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
